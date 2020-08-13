1542742bccb481f130ede8613e2a500a3948215e
jamiejamiebobamie <jmccrory@vt.edu>
2020-08-12 22:54:30 -0700

    pushign to heroku

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 15000;
-
+// initializing port
+const port = process.env.PORT || 15000;
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
