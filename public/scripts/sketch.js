5afab19a0cf2801c6eb23bcda9eb81a931b20d6d
jamiejamiebobamie <jmccrory@vt.edu>
2020-08-13 11:40:47 -0700

    added the drawing feature

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -6,6 +6,16 @@ const sketchMarginRatio = 1.1;
 let sketchSide;
 let fontStyle;
 
+let stroke = [];
+let strokes = [];
+
+let isDrawing = false;
+let getStroke = false;
+let strokeTimer;
+
+let x = 0;
+let y = 0;
+
 function preload(){
     // fontStyle = loadFont('../fonts/ArchitectsDaughter-Regular.tff');
 }
@@ -24,11 +34,51 @@ function draw() {
 
     // 2.) gif(s)
 
+    fill(100,0,0)
+
+    for (let i = 0; i < stroke.length;i++){
+        ellipse(stroke[i].x*sketchSide, stroke[i].y*sketchSide, sketchSide*.03,sketchSide*.03)
+    }
+
+    fill(0)
+
+    for (let i = 0; i < strokes.length;i++){
+        for (let j = 0; j < strokes[i].length;j++){
+            ellipse(strokes[i][j].x*sketchSide, strokes[i][j].y*sketchSide, sketchSide*.03,sketchSide*.03)
+        }
+    }
     // 3.) button to initiate sequence.
     // placeholder:
         // textFont(fontStyle)
         // offset the textbox / button by half the width of the textbox
-        text("click me.", sketchSide/2, sketchSide-sketchSide/7)
+        // text("click me.", sketchSide/2, sketchSide-sketchSide/7)
+    if (isDrawing){
+        buildStroke();
+    }
+}
+
+function mouseReleased() {
+    isDrawing = false;
+    strokes.push(stroke);
+    console.log(strokes)
+
+    stroke = [];
+
+    frameRate(24);
+
+    x = mouseX;
+    y = mouseY;
+}
+
+function mousePressed() {
+    if (!isDrawing){
+        isDrawing = true;
+        frameRate(70);
+    }
+}
+
+function buildStroke(){
+    stroke.push({x:mouseX/sketchSide,y:mouseY/sketchSide})
 }
 
 function windowResized() {
